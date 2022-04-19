067b49856120ebd35e4a60533733473842866204
AdamWox <adam.wozniak@troncomputers.pl>
2022-04-19 13:54:47 +0200

    INIT

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,64 @@
+<script>
+  import "../app.css";
+  import Sidebar from "flowbite-svelte";
+  import {
+    ChartPieIconSolid,
+    ShoppingBagIconSolid,
+    TableIconSolid,
+    InboxInIconSolid,
+    UserIconSolid,
+    LoginIconSolid,
+    CogIconSolid
+  } from '@codewithshin/svelte-heroicons';
+
+  let uid = 1;
+  let links = [
+    {
+      id: uid++,
+      name: 'Dashboard',
+      href: '/',
+      icon: ChartPieIconSolid
+    },
+    {
+      id: uid++,
+      name: 'E-commerce',
+      icon: ShoppingBagIconSolid,
+      href: '/',
+      subtext: '3'
+    },
+    {
+      id: uid++,
+      name: 'Kanban',
+      icon: TableIconSolid,
+      href: '/',
+      subtext: 'Pro'
+    },
+    {
+      id: uid++,
+      name: 'Inbox',
+      icon: InboxInIconSolid,
+      href: '/'
+    },
+    {
+      id: uid++,
+      name: 'Users',
+      icon: UserIconSolid,
+      href: '/'
+    },
+    {
+      id: uid++,
+      name: 'Sign In',
+      icon: LoginIconSolid,
+      href: '/'
+    },
+    {
+      id: uid++,
+      name: 'Setting',
+      icon: CogIconSolid,
+      href: '/'
+    }
+  ];
+</script>
+
+<Sidebar {links} />
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Navbar, Button } from "flowbite-svelte";
+  let siteName = "Flowbite Svelte";
+  let logo = "/images/flowbite-svelte-logo-30.png";
+  let alt = "flowbite-svelte";
+  let textsize = "text-lg";
+  let menus = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/shinokada/flowbite-svelte"
+    },
+    {
+      name: "Design",
+      href: "https://flowbite-svelte.vercel.app",
+    },
+  ];
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<Navbar {menus} {siteName} {alt} {logo} {textsize} />
+<Button name="Click Me!" />
